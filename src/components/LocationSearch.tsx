e3091e5322a60148879bdd1dac38ddc0ab7111cd
Yogesh Rana <yr70416@gmail.com>
2025-03-14 23:01:07 +0530

    Change text color black into searchBar and results

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -34,7 +34,7 @@ const LocationSearch = ({ onPlaceClick }: LocationSearchProps) => {
                     Search
                 </label>
                 <input
-                    className={`border ${error ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full`}
+                    className={`border ${error ? 'border-red-500' : 'border-gray-300'} rounded-md text-black shadow-sm focus:border-indigo-500 px-4 py-2 w-full`}
                     id='term'
                     value={term}
                     onChange={e => {
@@ -56,7 +56,7 @@ const LocationSearch = ({ onPlaceClick }: LocationSearchProps) => {
             <div className='grid grid-cols-[1fr_40px] gap-2 mt-2 items-center'>
                 {places.map(place => (
                     <Fragment key={place.id}>
-                        <p className='text-sm'>{place.name}</p>
+                        <p className='text-sm text-black'>{place.name}</p>
                         <button
                             className='bg-gradient-to-r from-blue-500 to-indigo-500 text-xs text-white font-bold py-1 px-2 rounded-full shadow-lg transform transition-transform duration-300 hover:scale-105 hover:from-blue-600 hover:to-indigo-600 h-8 w-12 flex items-center justify-center'
                             onClick={() => onPlaceClick(place)}
